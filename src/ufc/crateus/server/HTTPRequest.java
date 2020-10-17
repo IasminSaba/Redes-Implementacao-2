88bd390764e7571587e945bcc982e9bd8bc300d8
fcalex <contato.fcoalex@gmail.com>
2020-10-17 19:49:44 -0300

    adicao de get e correcoes

--- src/ufc/crateus/server/HTTPRequest.java
+++ src/ufc/crateus/server/HTTPRequest.java
@@ -2,8 +2,10 @@ package ufc.crateus.server;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -14,17 +16,14 @@ public class HTTPRequest implements Runnable {
 
 	final static String CRLF = "\r\n";
 	Socket socket;
-	
-	
-	
-	public HTTPRequest(Socket socket) throws Exception
-	{
+
+	public HTTPRequest(Socket socket) throws Exception {
 		this.socket = socket;
 	}
-	
+
 	@Override
 	public void run() {
-		
+
 		try {
 			processRequest();
 		} catch (Exception e) {
@@ -32,112 +31,141 @@ public class HTTPRequest implements Runnable {
 		}
 	}
 
-	
-	  private void processRequest() throws Exception {
-	        
-	        InputStream is = socket.getInputStream();
-	        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+	private void processRequest() throws Exception {
 
-	   
-	        String requestLine = br.readLine(); //linha request
-	        
-	        StringTokenizer tokens = new StringTokenizer(requestLine);
-	        tokens.nextToken(); 
-	        String file= tokens.nextToken();
+		InputStream is = socket.getInputStream();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-	        
-	        
-	        System.out.println("Nova requisição.");
-	        System.out.println(requestLine);
-	        String headerLine = null;
-	        while ((headerLine = br.readLine()).length() != 0) {
-	            System.out.println(headerLine);
-	        }
+		String requestLine = br.readLine(); // linha request
 
-	        getResourse(file, br);
-	        
-	    }
+		StringTokenizer tokens = new StringTokenizer(requestLine);
+		tokens.nextToken();
+		String file = tokens.nextToken();
 
-	  
-	  private void getResourse(String file, BufferedReader br ) throws Exception {
-		     DataOutputStream os = new DataOutputStream(socket.getOutputStream());
-		     boolean fileExists = true;
-		     
-		     
-		     file = "." + file; //Arquivo no diretório	        
-		        FileInputStream f = null;
-		       
-		        try {
-		            f = new FileInputStream(file);
-		        } catch (FileNotFoundException e) {
-		            fileExists = false;
-		        }
+		System.out.println("Nova requisicao...");
+		System.out.println(requestLine);
+		String headerLine = null;
+		while ((headerLine = br.readLine()).length() != 0) {
+			System.out.println(headerLine);
+		}
 
-		        
-		     String status = null;
-		        String content= null;
-		        String body = null;
-		        if (fileExists) {
-		            status= "HTTP/1.0 200 " + CRLF;
-		            content= "Content: "
-		                    + getContent(file) + CRLF;
-		        } else {
-		            status= "HTTP/1.0 404 Not Found" + CRLF;
-		            content= "Content-Type: index/html" + CRLF;
-		            body= "<HTML>"
-		                    + "<HEAD><TITLE>Not Found</TITLE></HEAD>"
-		                    + "<body>\n"
-		                    + "<div align=\"center\"><p><img alt=\"notFound\" src=\"../notFound.png\"\n"
-		                    + "width=\"350\"></p></div>\n"
-		                    + "</body></HTML>";
-		        }
-		        
+		getResourse(file, br);
 
-		        os.writeBytes(status);
-		        os.writeBytes(content);
+	}
 
-		        //Indicar o fim de header
-		        os.writeBytes(CRLF);
+	private void getResourse(String file, BufferedReader br) throws Exception {
+		
+		DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+		boolean fileExists = true;
+		FileInputStream f = null;
+		
+		file = "." + file;
 
-		        //Se o arquivo existir
-		        if (fileExists) {
-		            sendBytes(f, os);
-		            f.close();
-		        } else {
-		            os.writeBytes(body);
-		        }
+		try {
+			f = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			fileExists = false;
+		}
+		
+		String status = null;
+		String content = null;
+		String body = null;
+		
+		if(hasParams(file)) {
+			String res[] =  file.split("\\?");
+			String params[] = res[1].split("&");
+			
+			if(res[0].equals("./msg")) {
+				status = "HTTP/1.0 404 Not Found" + CRLF;
+				content = "Content-Type: text/html" + CRLF;
+				body = codeHtmlWithParams("." + File.separator + "resposta.html", params[0], params[1]);
+			}
+		} else {
+			
+			if (fileExists && file != "./error.html") {
+				status = "HTTP/1.0 200 OK" + CRLF;
+				content = "Content: " + getContent(file) + CRLF;
+			} else {
+				status = "HTTP/1.0 404 Not Found" + CRLF;
+				content = "Content-Type: text/html" + CRLF;
+				body = codeHtml("." + File.separator + "error.html");
+			}
+		}
 
-		       
-		        os.close();
-		        br.close();
-		        socket.close();
-	  }
-	  
-	  private static void sendBytes(FileInputStream fis,
-			  OutputStream os) throws Exception {
-	    
-		  byte[] buffer = new byte[1024];
-		  int bytes = 0;
-		  
-		  while ((bytes = fis.read(buffer)) != -1) {
-	          os.write(buffer, 0, bytes);
-	      }
-	  }
+		os.writeBytes(status);
+		os.writeBytes(content);
 
-	  private static String getContent(String file) {
-	      if (file.endsWith(".html")) {
-	          return "index/html";
-	      }
+		// Indicar o fim de header
+		os.writeBytes(CRLF);
 
-	      return "application/octet-stream";
-	  }
+		// Se o arquivo existir
+		if (fileExists) {
+			sendBytes(f, os);
+			f.close();
+		} else {
+			os.writeBytes(body);
+		}
+
+		os.close();
+		br.close();
+		socket.close();
+	}
+
+	private String codeHtmlWithParams(String file, String ... str) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		String linha, result = "";
+		while((linha = br.readLine()) != null) {
+			if(linha.contains("<div>")) {
+				result += linha + "\n";
+				for (String param : str) {
+					String values[] = param.split("=");
+					result += "<h3>" + values[0] + "</h2>";
+					result += "<p class=\"h5\">" + values[1].
+							replaceAll("\\+", " "). // espaco
+							replaceAll("%2C", ",") // virgula
+							+ "</p>";
+				}
+			} else {
+				result += linha + "\n";
+			}
+		}
+			
+		br.close();
+		return result;
+	}
+
+	private boolean hasParams(String file) {
+		String res[] =  file.split("\\?");
+		
+		if(res.length > 1) return true;
+		return false;
+	}
+
+	private String codeHtml(String file) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		String linha, result = "";
+		while((linha = br.readLine()) != null)
+			result += linha + "\n";
+		br.close();
+		return result;
+	}
+
+	private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception {
+
+		byte[] buffer = new byte[1024];
+		int bytes = 0;
+
+		while ((bytes = fis.read(buffer)) != -1) {
+			os.write(buffer, 0, bytes);
+		}
+	}
+
+	private static String getContent(String file) {
+		if (file.endsWith(".html")) {
+			return "text/html";
+		}
+
+		return "application/octet-stream";
+	}
 }
-
-
-
-
-
-
-
-
-
--- src/ufc/crateus/server/WebServer.java
+++ src/ufc/crateus/server/WebServer.java
@@ -4,19 +4,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class WebServer {
-	
-	public static void main (String args[] ) throws Exception{
-		int port = 3000;
-		
+
+	public static void main(String args[]) throws Exception {
+		int port = 5005;
+
 		@SuppressWarnings("resource")
 		ServerSocket socket = new ServerSocket(port);
-		
-		while(true) {
+
+		while (true) {
+			System.out.println("Rodando...");
 			Socket conne = socket.accept();
-			
+
 			HTTPRequest request = new HTTPRequest(conne);
-	
-			
+
 			Thread thread = new Thread(request);
 			thread.start();
 		}
